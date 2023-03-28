c46c325a9e7c4eb7466a8ae51aa07d4d188262c3
Berkay Akçay <berkayakcay@outlook.com>
2023-03-28 15:24:43 +0300

    Installing React Router v6 & Configuring the Router

---
+++ Ch07/07_01/start/react-app/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+export function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+    </div>
+  )
+}
+
+
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  )
+}
+
+
+export function App() {
+  return (
+    <h1>App</h1>
+  );
+}
